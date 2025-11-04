d0a66f4c90f590ef64dafa886395ccbd295c0ca3
Giri Prasad P <giriprasa@gmail.com>
2025-11-04 16:54:03 +0530

    first commit

---
+++ data_types.py
@@ -0,0 +1,35 @@
+# dynamic binding
+# w.r.t -> datatype -> 
+
+x = 100
+# x - variable name
+# = - assignment operator
+# 100 - value
+
+#  variablename = value
+# datatype variable = value;
+# int x = 10
+
+x = 10
+print(x)
+print(f"the value of x is {x}, and the datatype is {type(x)}")
+x = 10.0
+print(x)
+print(f"the value of x is {x}, and the datatype is {type(x)}")
+
+x = '10'
+print(x)
+print(f"the value of x is {x}, and the datatype is {type(x)}")
+
+x = '10.00'
+print(x)
+print(f"the value of x is {x}, and the datatype is {type(x)}")
+
+# True or False
+isAvailable = True 
+print("The product e10102 isAvailable ", isAvailable, "the datatype of isAvailable is ", type(isAvailable))
+print(f"The product e10102 isAvailable {isAvailable}, the datatype of isAvailable is {type(isAvailable)}")
+isAvailable = 'True' 
+print(f"The product e10102 isAvailable {isAvailable}, the datatype of isAvailable is {type(isAvailable)}")
+isAvailable = False
+print(f"The product e10102 isAvailable {isAvailable}, the datatype of isAvailable is {type(isAvailable)}")
---
+++ intro.md
@@ -0,0 +1,32 @@
+Python
+        -   Hign level 
+        - General application 
+        - web application
+        - Gaming Application
+        - Automation
+
+Easy to learn
+read
+
+Interpreted
+Portal ->  Mac | Windows | Linux
+Rich Library
+Functional 
+OPP
+
+    - Data processing 
+    - Data Analytics
+    - Processing 
+    - ML 
+    - Select the model 
+    - Train the model
+    - Prediction
+    - Automation
+    - Games
+    - Web 
+    - AI Application / ML Application
+
+Basic:
+    How to write & execute
+    Store variables
+    Data types

466d4625147660f0b686d4976918316b7c3e757e
Giri Prasad P <giriprasa@gmail.com>
2025-11-05 16:34:14 +0530

    set

---
+++ dt_set.py
@@ -0,0 +1,30 @@
+l1 = [1,23,4]
+t1 = tuple(l1)
+print(l1)
+print(*l1)
+print(t1)
+
+for items in l1:
+    print(items)
+l2 = [98,101]
+l1.append(l2)
+print(l1)
+
+
+set1 = {2,4,6,1,9,1,2,4,6,1,9,1}
+set2 = {1,3,5,7,9,2,4}
+set3 = {4,5,6,7}
+
+allgames = set3.intersection(set2).intersection(set1)
+print(allgames)
+
+
+allgames = set2.intersection(set3)
+print(allgames)
+
+tennisonly = set2 - set1 - set3
+print(tennisonly)
+
+
+onlytennis_onlyvoll = set1 ^ set2
+print(onlytennis_onlyvoll)

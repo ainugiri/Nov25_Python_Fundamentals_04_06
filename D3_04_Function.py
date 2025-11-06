17d5bd04deae92ca780ded3f4521ff9e56c4ffb0
Giri Prasad P <giriprasa@gmail.com>
2025-11-06 17:26:03 +0530

    Functions

---
+++ D3_04_Function.py
@@ -0,0 +1,36 @@
+# access_modifier return_type functionname (argu){
+# -------------
+# ---------
+# ---------
+# }
+
+# functionname(arguments)
+
+
+def functionname():
+    print("You called the function, following inst")
+    print("stmt1")
+    print("stmt2")
+    print("stmt3")
+    print("stmt4")
+    print("stmt5")
+functionname();
+
+
+
+def welcome(username="Guest"):
+    print(f"Hi {username}, Welcome to the training program")
+
+welcome("Giri");
+welcome("Steve");
+welcome();
+
+def addition(a,b=1):
+    return a/b
+
+print(addition(100))
+
+
+c_area = lambda x : 3.14 *x * x
+
+print(c_area(7))
